4feb6d2af56c1a9da38bbe94c8986572eef16cc3
Sandeep Kodag <Sandeep Kodag>
2018-07-11 10:33:17 +0530

    added route link

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
+
   title = 'Menu';
+
+  constructor(private router: Router) { }
 }
